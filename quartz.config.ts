ee8bb0042a85bf74f2333398b67663dfd663d13a
Tran Trung Hieu <35338689+R3p1ns@users.noreply.github.com>
2026-05-07 00:38:37 +0700

    gegge

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "r3p1ns.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
